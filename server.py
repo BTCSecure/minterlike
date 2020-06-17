40acdcf3ea61e573d039e33be709f63d922a0fc8
COMMASTER <admin@mcorp.space>
2020-06-17 16:21:51 +0300

    add nginx config

--- server.py
+++ server.py
@@ -3,15 +3,18 @@ from flask import render_template
 app = Flask(__name__)
 import tg_analytic
 
+
 class chat(object):
     name = None
     likes_sended = None
     messages = None
+
     def __init__(self, id):
         self.id = id
+
     def gets_data(self):
         return tg_analytic.get_chat(self.id)
-        
+
 
 @app.route('/')
 def hello_world():
@@ -19,14 +22,14 @@ def hello_world():
     r = []
     for i in chats_list:
         r.append(tg_analytic.get_chat(i))
-    return  render_template('index.html',r=r)
+    return render_template('index.html', r=r)
+
 
 @app.route('/stats')
 def stats():
     chats_list = tg_analytic.custom(foo=False)
     print(chats_list)
-
-    return  render_template('stats.html',stats=chats_list)
+    return render_template('stats.html', stats=chats_list)
 
 
 if __name__ == '__main__':
