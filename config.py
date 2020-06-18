84c4b82601f104805a6aa5d65c1e8f2554c394cd
COMMASTER <admin@mcorp.space>
2020-06-18 12:27:38 +0300

    add config

---
+++ config.py
@@ -0,0 +1,6 @@
+token = "token from bot father"
+api_id = "app id from https://my.telegram.org/apps"
+api_hash = "app hash from https://my.telegram.org/apps"
+db_password = '12345678'
+db_user = "minter_like_bot"
+threads = 32
